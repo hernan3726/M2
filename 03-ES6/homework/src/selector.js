98a11ad9335c814a78b805ef52b0a9281dea1e6a
hernan3726 <hernan_carp93@hotmail.com>
2022-11-29 02:57:39 -0300

    first commit

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,3 +1,5 @@
+const { arrayReplaceAt } = require("markdown-it/lib/common/utils");
+
 var traverseDomAndCollectElements = function(matchFunc, startEl) {
   var resultSet = [];
 
@@ -9,7 +11,14 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl)
+
+  for(let i =0; i<arrayReplaceAt.length;i++){
+    let res = traverseDomAndCollectElements(matchFunc, startEl.children[i])
+  //                  true o false 
+  resultSet =[...resultSet, ...res] // genero una copia de result y una copia de res
+  }
+  return resultSet
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +27,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === "#") return "id"
+  if(selector [0 === "."]) return "class" 
+  if(selector.split(".").length === 2) return "tag.clase"// cuando haya split lo parta en 2
+return "tag"
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -27,16 +39,31 @@ var selectorTypeMatcher = function(selector) {
 // matchea el selector.
 
 var matchFunctionMaker = function(selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
+  const selectorType = selectorTypeMatcher(selector);
+  let matchFunction;
   if (selectorType === "id") { 
-   
-  } else if (selectorType === "class") {
-    
+    matchFunctionMaker=function(elem){
+      return "#"+elem.id ===selector
+      // id two            #idTwo
+    }
+  } else if (selectorType === "class") {// <div className "classOne classRandom"
+    matchFunctionMaker=function(elem){
+      for(const clase of elem.classList) {
+        if("." + clase === selector) return true;
+        
+      } 
+      return false 
+      }
   } else if (selectorType === "tag.class") {
-    
-  } else if (selectorType === "tag") {
-    
+    matchFunctionMaker=function(elem){
+      return elem.tagName === selector.toUpperCase()// si tu DIV es igual a mi DIV haceme tal cosa
+      }
+  } else if (selectorType === "tag") {// div.classOne o h1.classRanadome, agarrame un arreglo y dividime en dos primera parte A y segunda B 
+    matchFunctionMaker=function(elem){
+      let [tag,clase] = selector.split(".")
+      return matchFunctionMaker(tag)(elem) &&matchFunctionMaker(clase)(elem)
+      //ejecutame matchfunctionmaker que sea con h1 o div
+      }
   }
   return matchFunction;
 };
